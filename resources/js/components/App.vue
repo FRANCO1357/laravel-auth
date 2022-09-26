b1b982a48335599d09208d58bd6583c0c17f420c
FRANCO1357 <fmelani13@gmail.com>
2022-09-26 16:39:31 +0200

    front vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="content">
+        <div class="title m-b-md">
+        Lavori in corso...
+        </div> 
+    </div>
+</template>
+
+<script>
+export default{
+    name: "App",
+};
+</script>
+
+<style scoped lang="scss"></style>
