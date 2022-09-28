9e1d553ba608ca2903bb135948c1367387c81013
FRANCO1357 <fmelani13@gmail.com>
2022-09-28 15:36:45 +0200

    validation selected

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -44,7 +44,8 @@ class PostController extends Controller
         $request->validate([
             'title' => 'required|string|min:5|max:50|unique:posts',
             'content' => 'required|string',
-            'image' => 'nullable|url'
+            'image' => 'nullable|url',
+            'category_id' => 'nullable|exists:categories,id',
         ],
         [
             'title.required' => 'Il titolo è obbligatorio',
@@ -53,7 +54,7 @@ class PostController extends Controller
             'title.unique' => "Il titolo $request->title esiste già",
             'content.required' => 'Il contenuto è obbligatorio',
             'image.required' => 'Il link dell\'immagine deve iniziare con http',
-
+            'category_id.exixts' => 'Categoria non esistente',
         ]);
 
         $data = $request->all();
